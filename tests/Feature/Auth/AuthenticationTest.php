337055104d70ddba129d32932bb981d1d21e6aca
aymanezariouh <aymanezariouh@gmail.com>
2026-02-26 10:40:42 +0000

    removed welcome page and fixed auth redirects

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -45,5 +45,5 @@ test('users can logout', function () {
     $response = $this->actingAs($user)->post('/logout');
 
     $this->assertGuest();
-    $response->assertRedirect('/');
+    $response->assertRedirect(route('login'));
 });
